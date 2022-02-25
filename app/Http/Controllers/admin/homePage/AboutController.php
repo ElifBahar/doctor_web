6dbfb393cfaf3617ca9c953f94aba85c6c37100d
Elif Bahar <elif.har94@gmail.com>
2022-02-26 01:29:48 +0300

    add update and delete methods to homepage-about controller, add homepage-about edit page

--- app/Http/Controllers/admin/homePage/AboutController.php
+++ app/Http/Controllers/admin/homePage/AboutController.php
@@ -30,4 +30,30 @@ class AboutController extends Controller
         return Redirect()->route('about-index')->with('success','Hakkımda Başaarıyla Oluşturuldu');
 
     }
+
+    public function AboutEdit($id){
+
+        $homeabout = HomeAbout::find($id);
+        return view('admin.homePage.about.edit', compact('homeabout'));
+    }
+
+    public function AboutUpdate(Request $request, $id){
+
+        $update = HomeAbout::find($id)->update([
+            'title' => $request->title,
+            'short_des' => $request->short_des,
+            'long_des' => $request->long_des,
+            'created_at' => Carbon::now()
+        ]);
+
+        return Redirect()->route('about-index')->with('success','Hakkında Başarıyla Güncellendi');
+
+    }
+
+    public function AboutDelete($id){
+        $delete = HomeAbout::find($id)->delete();
+
+        return Redirect()->back()->with('success','Hakkında başarıyla silindi.');
+
+    }
 }
