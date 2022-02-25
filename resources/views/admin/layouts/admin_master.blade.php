450e0dea783f08b0f683135e90c95ee9728cb305
Elif Bahar <elif.har94@gmail.com>
2022-02-25 22:14:58 +0300

    admin template master file edited - search bar removed

--- resources/views/admin/layouts/admin_master.blade.php
+++ resources/views/admin/layouts/admin_master.blade.php
@@ -54,7 +54,7 @@
 ——— LEFT SIDEBAR WITH FOOTER
 =====================================
 -->
-    @include('admin.body.sidebar')
+    @include('admin.layouts.admin_sidebar')
 
     <div class="page-wrapper">
         <!-- Header -->
@@ -66,87 +66,31 @@
                 </button>
                 <!-- search form -->
                 <div class="search-form d-none d-lg-inline-block">
-                    <div class="input-group">
-                        <button type="button" name="search" id="search-btn" class="btn btn-flat">
-                            <i class="mdi mdi-magnify"></i>
-                        </button>
-                        <input type="text" name="query" id="search-input" class="form-control" placeholder="'button', 'chart' etc."
-                               autofocus autocomplete="off" />
-                    </div>
-                    <div id="search-results-container">
-                        <ul id="search-results"></ul>
-                    </div>
+
                 </div>
 
                 <div class="navbar-right ">
                     <ul class="nav navbar-nav">
-                        <!-- Github Link Button -->
-                        <li class="github-link mr-3">
-                            <a class="btn btn-outline-secondary btn-sm" href="https://github.com/tafcoder/sleek-dashboard" target="_blank">
-                                <span class="d-none d-md-inline-block mr-2">Source Code</span>
-                                <i class="mdi mdi-github-circle"></i>
-                            </a>
 
-                        </li>
-                        <li class="dropdown notifications-menu">
-                            <button class="dropdown-toggle" data-toggle="dropdown">
-                                <i class="mdi mdi-bell-outline"></i>
-                            </button>
-                            <ul class="dropdown-menu dropdown-menu-right">
-                                <li class="dropdown-header">You have 5 notifications</li>
-                                <li>
-                                    <a href="#">
-                                        <i class="mdi mdi-account-plus"></i> New user registered
-                                        <span class=" font-size-12 d-inline-block float-right"><i class="mdi mdi-clock-outline"></i> 10 AM</span>
-                                    </a>
-                                </li>
-                                <li>
-                                    <a href="#">
-                                        <i class="mdi mdi-account-remove"></i> User deleted
-                                        <span class=" font-size-12 d-inline-block float-right"><i class="mdi mdi-clock-outline"></i> 07 AM</span>
-                                    </a>
-                                </li>
-                                <li>
-                                    <a href="#">
-                                        <i class="mdi mdi-chart-areaspline"></i> Sales report is ready
-                                        <span class=" font-size-12 d-inline-block float-right"><i class="mdi mdi-clock-outline"></i> 12 PM</span>
-                                    </a>
-                                </li>
-                                <li>
-                                    <a href="#">
-                                        <i class="mdi mdi-account-supervisor"></i> New client
-                                        <span class=" font-size-12 d-inline-block float-right"><i class="mdi mdi-clock-outline"></i> 10 AM</span>
-                                    </a>
-                                </li>
-                                <li>
-                                    <a href="#">
-                                        <i class="mdi mdi-server-network-off"></i> Server overloaded
-                                        <span class=" font-size-12 d-inline-block float-right"><i class="mdi mdi-clock-outline"></i> 05 AM</span>
-                                    </a>
-                                </li>
-                                <li class="dropdown-footer">
-                                    <a class="text-center" href="#"> View All </a>
-                                </li>
-                            </ul>
-                        </li>
+
                         <!-- User Account -->
                         <li class="dropdown user-menu">
                             <button href="#" class="dropdown-toggle nav-link" data-toggle="dropdown">
-                                <img src="{{ asset('backend/assets/img/user/user.png') }}" class="user-image" alt="User Image" />
-                                <span class="d-none d-lg-inline-block">Abdus Salam</span>
+                                <img src="{{ Auth::user()->profile_photo_url }}" class="user-image" alt="{{ Auth::user()->name }}" />
+                                <span class="d-none d-lg-inline-block">{{ Auth::user()->name }}</span>
                             </button>
                             <ul class="dropdown-menu dropdown-menu-right">
                                 <!-- User image -->
                                 <li class="dropdown-header">
-                                    <img src="{{ asset('backend/assets/img/user/user.png') }}" class="img-circle" alt="User Image" />
+                                    <img src="{{ Auth::user()->profile_photo_url }}" class="img-circle" alt="{{ Auth::user()->name }}" />
                                     <div class="d-inline-block">
-                                        Abdus Salam <small class="pt-1">abdus@gmail.com</small>
+                                        {{ Auth::user()->name }} <small class="pt-1">abdus@gmail.com</small>
                                     </div>
                                 </li>
 
                                 <li>
-                                    <a href="profile.html">
-                                        <i class="mdi mdi-account"></i> My Profile
+                                    <a href="{{ route('profile.show') }}">
+                                        <i class="mdi mdi-account"></i> Profilim
                                     </a>
                                 </li>
                                 <li>
@@ -154,15 +98,13 @@
                                         <i class="mdi mdi-email"></i> Message
                                     </a>
                                 </li>
-                                <li>
-                                    <a href="#"> <i class="mdi mdi-diamond-stone"></i> Projects </a>
-                                </li>
+
                                 <li>
                                     <a href="#"> <i class="mdi mdi-settings"></i> Account Setting </a>
                                 </li>
 
                                 <li class="dropdown-footer">
-                                    <a href="{{ route('user.logout') }}"> <i class="mdi mdi-logout"></i> Log Out </a>
+                                    <a href="{{ route('logout') }}"> <i class="mdi mdi-logout"></i> Log Out </a>
                                 </li>
                             </ul>
                         </li>
