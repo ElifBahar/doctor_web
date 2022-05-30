ba7d5d8bceef6ba9accb427274e15c75400e64d8
abdulskaya <abdulsamet.kaya@pixasoftware.com>
2022-05-31 01:20:43 +0300

    Update frontend

---
+++ resources/views/frontend/layouts/app.blade.php
@@ -0,0 +1,138 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="frontend/assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="frontend/assets/css/reset.css">
+    <link rel="stylesheet" href="frontend/assets/css/style.css">
+    <link rel="stylesheet" href="frontend/assets/js/bootstrap.min.js">
+    <link rel="stylesheet" href="frontend/assets/js/popper.min.js">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+</head>
+<body>
+<div class="header">
+    <nav>
+        <div class="nav-div" id="nav-left">
+            <span id="logo">Ömer C. Batur</span>
+        </div>
+        <div class="nav-div" id="nav-right">
+            <ul>
+                <li class="nav-right-links"><a href="">Ana Sayfa</a></li>
+                <li class="nav-right-links"><a href="">Hakkımda</a></li>
+                <li class="nav-right-links"><a href="">Uygulamalar</a></li>
+                <li class="nav-right-links"><a href="">BİLGİ KÖŞESİ</a></li>
+                <li class="nav-right-links"><a href="">Blog</a></li>
+                <li class="nav-right-links"><a href="">İLETİŞİM</a></li>
+                <li class="nav-right-links mx-0"><a href="" class="search"><img src="frontend/assets/img/search.png" alt="search.png"></a></li>
+                <li class="nav-right-links"><a href="" class="date-button"> <img src="frontend/assets/img/calendar-4.svg" alt="calendar-4.svg"> Randevu Al</a></li>
+            </ul>
+        </div>
+    </nav>
+
+    <div class="header-content-slider mx-auto">
+        <div class="text-area">
+                <span class="title">
+                    Op. Dr.
+                </span>
+            <h1 class="name">
+                Ömer Cihan BATUR
+            </h1>
+        </div>
+        <div class="header-cards">
+            <a href="" class="card1 card-standart">
+                <div>
+                    <h2>Lorem Ipsum Doları</h2>
+                    <div class="icon-border">
+                        <i class="fa-solid fa-arrow-right"></i>
+                    </div>
+                </div>
+                <i class="fa-solid fa-heart-circle-bolt"></i>
+            </a>
+            <a href="" class="card2 card-standart">
+                <div>
+                    <h2>Lorem Ipsum Doları</h2>
+                    <div class="icon-border">
+                        <i class="fa-solid fa-arrow-right"></i>
+                    </div>
+                </div>
+                <i class="fa-solid fa-calendar-check"></i>
+            </a>
+            <a href="" class="card3 card-standart">
+                <div>
+                    <h2>Randevu Al</h2>
+                    <div class="icon-border">
+                        <i class="fa-solid fa-arrow-right"></i>
+                    </div>
+                </div>
+                <div>
+                    <i class="fa-solid fa-phone"></i>
+                    <div>
+                        <span>Sağlık Parkı</span>
+                        <span>424</span>
+                    </div>
+                </div>
+            </a>
+        </div>
+    </div>
+</div>
+
+@yield('content')
+
+<div class="footer">
+    <div class="footer-inner">
+        <div class="footer-top">
+            <div>
+                <a href="" class="drname-logo">Ömer C. Batur</a>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nemo, ducimus.</p>
+            </div>
+            <div>
+                <h7 class="title">Hizmetler</h7>
+                <p>Lorem, ipsum.</p>
+                <p>Lorem, ipsum.</p>
+                <p>Lorem, ipsum.</p>
+                <p>Lorem, ipsum.</p>
+                <p>Lorem, ipsum.</p>
+            </div>
+            <div>
+                <h7 class="title">Fiyatlar</h7>
+                <p>400 &#8378</p>
+                <p>400 &#8378</p>
+                <p>400 &#8378</p>
+                <p>400 &#8378</p>
+                <p>400 &#8378</p>
+            </div>
+            <div>
+                <h7 class="title">İletişim</h7>
+                <div class="socialbtns">
+                    <ul>
+                        <li><a href="#" class="fab fa-linkedin-in"></a></li>
+                        <li><a href="#" class="fab fa-twitter"></a></li>
+                        <li><a href="#" class="fab fa-instagram"></a></li>
+                    </ul>
+                </div>
+                <div class="extra">
+                    <a target="_blank" href="tel:05399157309"><i class="fa-solid fa-phone"></i> +90 539 915 73 09</a>
+                    <a target="_blank" href="mailto:x@gmail.com"><i class="fa-solid fa-envelope"></i> x@gmail.com</i></a>
+                    <a target="_blank" href="https://www.google.com/maps/place/El%C3%A2z%C4%B1%C4%9F,+El%C3%A2z%C4%B1%C4%9F+Merkez%2FElaz%C4%B1%C4%9F/@38.6646596,39.198152,13z/data=!3m1!4b1!4m5!3m4!1s0x4076c06c76eaf697:0x12dab4118a3d2f39!8m2!3d38.674816!4d39.222515"><i class="fa-solid fa-location-dot"></i> Merkez / Elazığ</i></a>
+                </div>
+            </div>
+        </div>
+        <hr>
+        <div class="foter-bottom">
+            &#169;2022 <a target="_blank" href="">BJA SOFT</a>. Tüm Hakları Saklıdır.
+        </div>
+    </div>
+</div>
+</body>
+
+<script>
+    window.addEventListener("scroll",function(){
+        var navbar = this.document.querySelector("nav");
+        navbar.classList.toggle("sticky-nav", window.scrollY > 200);
+    })
+</script>
+</html>
---
+++ resources/views/frontend/xhome.blade.php
@@ -0,0 +1,321 @@
+@extends('frontend.layouts.frontend_master')
+@if( count($sliders) > 0 )
+
+    @include('frontend.layouts.slider')
+@endif
+
+@section('home_content')
+    <!-- ======= About Us Section ======= -->
+    <section id="about-us" class="about-us" >
+        <div class="container" data-aos="fade-up">
+
+            <div class="section-title">
+                <h2>Hakkımda</strong></h2>
+            </div>
+
+            <div class="row content">
+                <div class="col-lg-6" data-aos="fade-right">
+
+                    <div class="member aos-init aos-animate" data-aos="fade-up">
+                        <div class="member-img">
+
+                            <img src="{{asset('frontend/assets/img/team/team-1.jpg')}}" class="img-fluid" alt="">
+                            <div class="social">
+
+                            </div>
+                        </div>
+                        <div class="member-info">
+                            <h4>Walter White</h4>
+                            <span>Chief Executive Officer</span>
+                        </div>
+                    </div>
+
+                </div>
+                <div class="col-lg-6 pt-4 pt-lg-0" data-aos="fade-left">
+                    @if($about_me != null)
+
+                        <h2>{{$about_me->title}}</h2>
+                        <h3>Voluptatem dignissimos provident quasi corporis voluptates sit assum perenda sruen jonee trave</h3>
+
+                        <p>
+                            {{$about_me->short_des}}
+                        </p>
+
+                        <p>
+                            {{$about_me->long_des}}
+                        </p>
+
+                    @endif
+                </div>
+            </div>
+
+        </div>
+    </section><!-- End About Us Section -->
+
+    <!-- ======= Services Section ======= -->
+    <section id="services" class="services section-bg">
+        <div class="container" data-aos="fade-up" >
+
+            <div class="section-title">
+                <h2>Services</strong></h2>
+                <p>Laborum repudiandae omnis voluptatum consequatur mollitia ea est voluptas ut</p>
+            </div>
+
+            <div class="row">
+                <div class="col-lg-4 col-md-6 d-flex align-items-stretch" data-aos="zoom-in" data-aos-delay="100">
+                    <div class="icon-box iconbox-blue">
+                        <div class="icon">
+                            <svg width="100" height="100" viewBox="0 0 600 600" xmlns="http://www.w3.org/2000/svg">
+                            </svg>
+                            <i class="bx bxl-dribbble"></i>
+                        </div>
+                        <h4><a href="">Lorem Ipsum</a></h4>
+                        <p>Voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi</p>
+                    </div>
+                </div>
+
+                <div class="col-lg-4 col-md-6 d-flex align-items-stretch mt-4 mt-md-0" data-aos="zoom-in" data-aos-delay="200">
+                    <div class="icon-box iconbox-orange ">
+                        <div class="icon">
+                            <svg width="100" height="100" viewBox="0 0 600 600" xmlns="http://www.w3.org/2000/svg">
+                            </svg>
+                            <i class="bx bx-file"></i>
+                        </div>
+                        <h4><a href="">Sed Perspiciatis</a></h4>
+                        <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore</p>
+                    </div>
+                </div>
+
+                <div class="col-lg-4 col-md-6 d-flex align-items-stretch mt-4 mt-lg-0" data-aos="zoom-in" data-aos-delay="300">
+                    <div class="icon-box iconbox-pink">
+                        <div class="icon">
+                            <svg width="100" height="100" viewBox="0 0 600 600" xmlns="http://www.w3.org/2000/svg">
+                            </svg>
+                            <i class="bx bx-tachometer"></i>
+                        </div>
+                        <h4><a href="">Magni Dolores</a></h4>
+                        <p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia</p>
+                    </div>
+                </div>
+
+                <div class="col-lg-4 col-md-6 d-flex align-items-stretch mt-4" data-aos="zoom-in" data-aos-delay="100">
+                    <div class="icon-box iconbox-yellow">
+                        <div class="icon">
+                            <svg width="100" height="100" viewBox="0 0 600 600" xmlns="http://www.w3.org/2000/svg">
+                            </svg>
+                            <i class="bx bx-layer"></i>
+                        </div>
+                        <h4><a href="">Nemo Enim</a></h4>
+                        <p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis</p>
+                    </div>
+                </div>
+
+                <div class="col-lg-4 col-md-6 d-flex align-items-stretch mt-4" data-aos="zoom-in" data-aos-delay="200">
+                    <div class="icon-box iconbox-red">
+                        <div class="icon">
+                            <svg width="100" height="100" viewBox="0 0 600 600" xmlns="http://www.w3.org/2000/svg">
+                            </svg>
+                            <i class="bx bx-slideshow"></i>
+                        </div>
+                        <h4><a href="">Dele Cardo</a></h4>
+                        <p>Quis consequatur saepe eligendi voluptatem consequatur dolor consequuntur</p>
+                    </div>
+                </div>
+
+                <div class="col-lg-4 col-md-6 d-flex align-items-stretch mt-4" data-aos="zoom-in" data-aos-delay="300">
+                    <div class="icon-box iconbox-teal">
+                        <div class="icon">
+                            <svg width="100" height="100" viewBox="0 0 600 600" xmlns="http://www.w3.org/2000/svg">
+                            </svg>
+                            <i class="bx bx-arch"></i>
+                        </div>
+                        <h4><a href="">Divera Don</a></h4>
+                        <p>Modi nostrum vel laborum. Porro fugit error sit minus sapiente sit aspernatur</p>
+                    </div>
+                </div>
+
+            </div>
+
+        </div>
+    </section><!-- End Services Section -->
+
+    <!-- ======= Portfolio Section ======= -->
+    <section id="portfolio" class="portfolio">
+        <div class="container">
+
+            <div class="section-title" data-aos="fade-up">
+                <h2>Portfolio</h2>
+            </div>
+
+            <div class="row" data-aos="fade-up">
+                <div class="col-lg-12 d-flex justify-content-center">
+                    <ul id="portfolio-flters">
+                        <li data-filter="*" class="filter-active">All</li>
+                        <li data-filter=".filter-app">App</li>
+                        <li data-filter=".filter-card">Card</li>
+                        <li data-filter=".filter-web">Web</li>
+                    </ul>
+                </div>
+            </div>
+
+            <div class="row portfolio-container" data-aos="fade-up">
+
+                <div class="col-lg-4 col-md-6 portfolio-item filter-app">
+                    <img src="{{ asset('frontend/assets/img/portfolio/portfolio-1.jpg') }}" class="img-fluid" alt="">
+                    <div class="portfolio-info">
+                        <h4>App 1</h4>
+                        <p>App</p>
+                        <a href="{{ asset('frontend/assets/img/portfolio/portfolio-1.jpg') }}" data-gall="portfolioGallery" class="venobox preview-link" title="App 1"><i class="bx bx-plus"></i></a>
+                        <a href="portfolio-details.html" class="details-link" title="More Details"><i class="bx bx-link"></i></a>
+                    </div>
+                </div>
+
+                <div class="col-lg-4 col-md-6 portfolio-item filter-web">
+                    <img src="{{ asset('frontend/assets/img/portfolio/portfolio-2.jpg') }}" class="img-fluid" alt="">
+                    <div class="portfolio-info">
+                        <h4>Web 3</h4>
+                        <p>Web</p>
+                        <a href="{{ asset('frontend/assets/img/portfolio/portfolio-2.jpg') }}" data-gall="portfolioGallery" class="venobox preview-link" title="Web 3"><i class="bx bx-plus"></i></a>
+                        <a href="portfolio-details.html" class="details-link" title="More Details"><i class="bx bx-link"></i></a>
+                    </div>
+                </div>
+
+                <div class="col-lg-4 col-md-6 portfolio-item filter-app">
+                    <img src="{{ asset('frontend/assets/img/portfolio/portfolio-3.jpg') }}" class="img-fluid" alt="">
+                    <div class="portfolio-info">
+                        <h4>App 2</h4>
+                        <p>App</p>
+                        <a href="{{ asset('frontend/assets/img/portfolio/portfolio-3.jpg') }}" data-gall="portfolioGallery" class="venobox preview-link" title="App 2"><i class="bx bx-plus"></i></a>
+                        <a href="portfolio-details.html" class="details-link" title="More Details"><i class="bx bx-link"></i></a>
+                    </div>
+                </div>
+
+                <div class="col-lg-4 col-md-6 portfolio-item filter-card">
+                    <img src="{{ asset('frontend/assets/img/portfolio/portfolio-4.jpg') }}" class="img-fluid" alt="">
+                    <div class="portfolio-info">
+                        <h4>Card 2</h4>
+                        <p>Card</p>
+                        <a href="{{ asset('frontend/assets/img/portfolio/portfolio-4.jpg') }}" data-gall="portfolioGallery" class="venobox preview-link" title="Card 2"><i class="bx bx-plus"></i></a>
+                        <a href="portfolio-details.html" class="details-link" title="More Details"><i class="bx bx-link"></i></a>
+                    </div>
+                </div>
+
+                <div class="col-lg-4 col-md-6 portfolio-item filter-web">
+                    <img src="{{ asset('frontend/assets/img/portfolio/portfolio-5.jpg') }}" class="img-fluid" alt="">
+                    <div class="portfolio-info">
+                        <h4>Web 2</h4>
+                        <p>Web</p>
+                        <a href="{{ asset('frontend/assets/img/portfolio/portfolio-5.jpg') }}" data-gall="portfolioGallery" class="venobox preview-link" title="Web 2"><i class="bx bx-plus"></i></a>
+                        <a href="portfolio-details.html" class="details-link" title="More Details"><i class="bx bx-link"></i></a>
+                    </div>
+                </div>
+
+                <div class="col-lg-4 col-md-6 portfolio-item filter-app">
+                    <img src="{{ asset('frontend/assets/img/portfolio/portfolio-6.jpg') }}" class="img-fluid" alt="">
+                    <div class="portfolio-info">
+                        <h4>App 3</h4>
+                        <p>App</p>
+                        <a href="{{ asset('frontend/assets/img/portfolio/portfolio-6.jpg') }}" data-gall="portfolioGallery" class="venobox preview-link" title="App 3"><i class="bx bx-plus"></i></a>
+                        <a href="portfolio-details.html" class="details-link" title="More Details"><i class="bx bx-link"></i></a>
+                    </div>
+                </div>
+
+                <div class="col-lg-4 col-md-6 portfolio-item filter-card">
+                    <img src="{{ asset('frontend/assets/img/portfolio/portfolio-7.jpg') }}" class="img-fluid" alt="">
+                    <div class="portfolio-info">
+                        <h4>Card 1</h4>
+                        <p>Card</p>
+                        <a href="{{ asset('frontend/assets/img/portfolio/portfolio-7.jpg') }}" data-gall="portfolioGallery" class="venobox preview-link" title="Card 1"><i class="bx bx-plus"></i></a>
+                        <a href="portfolio-details.html" class="details-link" title="More Details"><i class="bx bx-link"></i></a>
+                    </div>
+                </div>
+
+                <div class="col-lg-4 col-md-6 portfolio-item filter-card">
+                    <img src="{{ asset('frontend/assets/img/portfolio/portfolio-8.jpg') }}" class="img-fluid" alt="">
+                    <div class="portfolio-info">
+                        <h4>Card 3</h4>
+                        <p>Card</p>
+                        <a href="{{ asset('frontend/assets/img/portfolio/portfolio-8.jpg') }}" data-gall="portfolioGallery" class="venobox preview-link" title="Card 3"><i class="bx bx-plus"></i></a>
+                        <a href="portfolio-details.html" class="details-link" title="More Details"><i class="bx bx-link"></i></a>
+                    </div>
+                </div>
+
+                <div class="col-lg-4 col-md-6 portfolio-item filter-web">
+                    <img src="{{ asset('frontend/assets/img/portfolio/portfolio-9.jpg') }}" class="img-fluid" alt="">
+                    <div class="portfolio-info">
+                        <h4>Web 3</h4>
+                        <p>Web</p>
+                        <a href="{{ asset('frontend/assets/img/portfolio/portfolio-9.jpg') }}" data-gall="portfolioGallery" class="venobox preview-link" title="Web 3"><i class="bx bx-plus"></i></a>
+                        <a href="portfolio-details.html" class="details-link" title="More Details"><i class="bx bx-link"></i></a>
+                    </div>
+                </div>
+
+            </div>
+
+        </div>
+    </section><!-- End Portfolio Section -->
+
+    <!-- ======= Our Clients Section ======= -->
+    <section id="clients" class="clients">
+        <div class="container" data-aos="fade-up">
+
+            <div class="section-title">
+                <h2>Clients</h2>
+            </div>
+
+            <div class="row no-gutters clients-wrap clearfix" data-aos="fade-up">
+
+                <div class="col-lg-3 col-md-4 col-6">
+                    <div class="client-logo">
+                        <img src="{{ asset('frontend/assets/img/clients/client-1.png') }}" class="img-fluid" alt="">
+                    </div>
+                </div>
+
+                <div class="col-lg-3 col-md-4 col-6">
+                    <div class="client-logo">
+                        <img src="{{ asset('frontend/assets/img/clients/client-2.png') }}" class="img-fluid" alt="">
+                    </div>
+                </div>
+
+                <div class="col-lg-3 col-md-4 col-6">
+                    <div class="client-logo">
+                        <img src="{{ asset('frontend/assets/img/clients/client-3.png') }}" class="img-fluid" alt="">
+                    </div>
+                </div>
+
+                <div class="col-lg-3 col-md-4 col-6">
+                    <div class="client-logo">
+                        <img src="{{ asset('frontend/assets/img/clients/client-4.png') }}" class="img-fluid" alt="">
+                    </div>
+                </div>
+
+                <div class="col-lg-3 col-md-4 col-6">
+                    <div class="client-logo">
+                        <img src="{{ asset('frontend/assets/img/clients/client-5.png') }}" class="img-fluid" alt="">
+                    </div>
+                </div>
+
+                <div class="col-lg-3 col-md-4 col-6">
+                    <div class="client-logo">
+                        <img src="{{ asset('frontend/assets/img/clients/client-6.png') }}" class="img-fluid" alt="">
+                    </div>
+                </div>
+
+                <div class="col-lg-3 col-md-4 col-6">
+                    <div class="client-logo">
+                        <img src="{{ asset('frontend/assets/img/clients/client-7.png') }}" class="img-fluid" alt="">
+                    </div>
+                </div>
+
+                <div class="col-lg-3 col-md-4 col-6">
+                    <div class="client-logo">
+                        <img src="{{ asset('frontend/assets/img/clients/client-8.png') }}" class="img-fluid" alt="">
+                    </div>
+                </div>
+
+            </div>
+
+        </div>
+    </section><!-- End Our Clients Section -->
+
+@endsection
