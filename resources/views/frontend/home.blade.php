678dbecdd5a846792d31ef17f182392cfe5586f8
ElifBahar <elif.har94@gmail.com>
2022-06-04 13:59:20 +0300

    frontend update

--- resources/views/frontend/home.blade.php
+++ resources/views/frontend/home.blade.php
@@ -1,115 +1,200 @@
 @extends('frontend.layouts.app')
 
 @section('content')
-    
-<div class="working-info">
-    <div class="working-info-inner z3 _inner">
-        <div class="working-times">
-            <h4>Çalışma Saatleri</h4>
-            <div class="day">
-                <span class="day-name">Pazartesi</span>
-                <span class="at-time">08.00 - 15.00 <a href="" class="take-meet"> <i class="fa-solid fa-calendar-check"></i> Randevu Al </a></span>
-            </div>
-            <div class="day">
-                <span class="day-name">Pazartesi</span>
-                <span class="at-time">08.00 - 15.00 <a href="" class="take-meet"> <i class="fa-solid fa-calendar-check"></i> Randevu Al </a></span>
-            </div>
-            <div class="day">
-                <span class="day-name">Pazartesi</span>
-                <span class="at-time">08.00 - 15.00 <a href="" class="take-meet"> <i class="fa-solid fa-calendar-check"></i> Randevu Al </a></span>
-            </div>
-            <div class="day">
-                <span class="day-name">Pazartesi</span>
-                <span class="at-time">İzinli</span>
-            </div>
-            <div class="day">
-                <span class="day-name">Pazartesi</span>
-                <span class="at-time">İzinli</span>
-            </div>
-        </div>
 
-        <a href="" class="work-type ">
-            <div class="work-face">
-                <img src="frontend/assets/img/card_image.jpg" alt="card_image.jpg">
-            </div>
-            <div class="details">
-                <div class="icon"><i class="fa-solid fa-shield-virus"></i></div>
-                <h1 class="title">Lorem, ipsum dolor.</h1>
-                <p class="content">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Illum et mollitia eius expedita eum?</p>
-                <span>Detaylar  <i class="fa-solid fa-arrow-right"></i></span>
-            </div>
-        </a>
-        <a href="" class="work-type">
-            <div class="work-face">
-                <img src="frontend/assets/img/card_image.jpg" alt="card_image.jpg">
-            </div>
-            <div class="details">
-                <div class="icon"><i class="fa-solid fa-shield-virus"></i></div>
-                <h1 class="title">Lorem, ipsum dolor.</h1>
-                <p class="content">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Illum et mollitia eius expedita eum?</p>
-                <span>Detaylar  <i class="fa-solid fa-arrow-right"></i></span>
-            </div>
-        </a>
-    </div>
-</div>
 
-<div class="homeAboutMe">
-    <div class="homeAboutMeInner _inner">
-        <div id="homeAboutMePic">
-            <img src="frontend/assets/img/O.png" alt="">
-        </div>
-        <div id="homeAboutMeRight">
-            <img class="tag" src="frontend/assets/img/tag.png" alt="tag.png">
-            <p>
-                İlk, orta ve lise öğrenimimi Elazığ’da tamamladıktan sonra <b>Fırat Üniversitesi Tıp Fakültesi</b>‘ni bitirdim. Daha sonra Tıpta Uzmanlık Sınavını kazanarak yine <b>Fırat Üniversitesi Tıp Fakültesi Deri ve Zührevi Hastalıkları Anabilim Dalı</b>’nda uzmanlık eğitimime başladım.
-                Uzmanlık eğitimini tamamladıktan sonra sırasıyla <b>Elazığ Deri ve Zührevi Hastalıklar Hastanesi</b> ve <b>Elazığ Devlet Hastanesi</b>’nde uzman doktor olarak çalıştım. Daha sonra <b>Özel Çağrı Tıp Merkezi</b> ve <b>Elazığ Medikal Park Hastaneleri</b>'nde daha sonra ise kendi kliniğimi kurarak çalışmaya devam ettim ve halen hastalarıma <b>kendi kliniğimde</b> hizmet vermeye devam etmekteyim.
-            </p>
-            <h2 class="drname">Op.Dr.Ömer Cihan Batur</h2>
-            <span class="script-text">Ortopedi ve Travmotoloji</span>
+    <div class="working-info">
+        <div class="working-info-inner z3 _inner">
+            <div class="working-times">
+                <h4>Çalışma Saatleri</h4>
+                <div class="day">
+                    <span class="day-name">Pazartesi</span>
+                    <span class="at-time">08.00 - 15.00 <a href="" class="take-meet"> <i
+                                class="fa-solid fa-calendar-check"></i> Randevu Al </a></span>
+                </div>
+                <div class="day">
+                    <span class="day-name">Pazartesi</span>
+                    <span class="at-time">08.00 - 15.00 <a href="" class="take-meet"> <i
+                                class="fa-solid fa-calendar-check"></i> Randevu Al </a></span>
+                </div>
+                <div class="day">
+                    <span class="day-name">Pazartesi</span>
+                    <span class="at-time">08.00 - 15.00 <a href="" class="take-meet"> <i
+                                class="fa-solid fa-calendar-check"></i> Randevu Al </a></span>
+                </div>
+                <div class="day">
+                    <span class="day-name">Pazartesi</span>
+                    <span class="at-time">İzinli</span>
+                </div>
+                <div class="day">
+                    <span class="day-name">Pazartesi</span>
+                    <span class="at-time">İzinli</span>
+                </div>
+            </div>
+
+            <a href="" class="work-type ">
+                <div class="work-face">
+                    <img src="frontend/assets/img/card_image.jpg" alt="card_image.jpg">
+                </div>
+                <div class="details">
+                    <div class="icon"><i class="fa-solid fa-shield-virus"></i></div>
+                    <h1 class="title">Klinik Tedaviler</h1>
+                    <p class="content">Tam teşekküllü ortopedi kliniziğimizde bir çok tedavi uygulanmaktadır</p>
+                    <span>Tümünü Görüntüle  <i class="fa-solid fa-arrow-right"></i></span>
+                </div>
+            </a>
+            <a href="" class="work-type">
+                <div class="work-face">
+                    <img src="frontend/assets/img/card_image.jpg" alt="card_image.jpg">
+                </div>
+                <div class="details">
+                    <div class="icon"><i class="fa-solid fa-shield-virus"></i></div>
+                    <h1 class="title">Cerrahi Tedaviler</h1>
+                    <p class="content">Ortopedik cerrahi yöntemleriyle bir çok tedavi yöntemi uygulamaktayız</p>
+                    <span>Tümünü Görüntüle  <i class="fa-solid fa-arrow-right"></i></span>
+                </div>
+            </a>
         </div>
     </div>
-</div>
 
-
-<div class="services">
-    <div class="services-inner _inner">
-        <div class="service-type">
-            <i class="fa-solid fa-brain"></i>
-            <h7 class="service-title">Lorem ipsum dolor sit amet.</h7>
-            <hr>
-            <p class="service-description">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Porro deserunt, eius at beatae cum provident?</p>
-            <ul>
-                <li><i class="fa-solid fa-circle-check"></i> Lorem, ipsum dolor.</li>
-                <li><i class="fa-solid fa-circle-check"></i> Lorem, ipsum dolor.</li>
-                <li><i class="fa-solid fa-circle-check"></i> Lorem, ipsum dolor.</li>
-            </ul>
-        </div>
-
-        <div class="service-type">
-            <i class="fa-solid fa-laptop-medical"></i>
-            <h7 class="service-title">Lorem ipsum dolor sit amet.</h7>
-            <hr>
-            <p class="service-description">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Porro deserunt, eius at beatae cum provident?</p>
-            <ul>
-                <li><i class="fa-solid fa-circle-check"></i> Lorem, ipsum dolor.</li>
-                <li><i class="fa-solid fa-circle-check"></i> Lorem, ipsum dolor.</li>
-                <li><i class="fa-solid fa-circle-check"></i> Lorem, ipsum dolor.</li>
-            </ul>
-        </div>
-
-        <div class="service-type">
-            <i class="fa-solid fa-house-medical"></i>
-            <h7 class="service-title">Lorem ipsum dolor sit amet.</h7>
-            <hr>
-            <p class="service-description">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Porro deserunt, eius at beatae cum provident?</p>
-            <ul>
-                <li><i class="fa-solid fa-circle-check"></i> Lorem, ipsum dolor.</li>
-                <li><i class="fa-solid fa-circle-check"></i> Lorem, ipsum dolor.</li>
-                <li><i class="fa-solid fa-circle-check"></i> Lorem, ipsum dolor.</li>
-            </ul>
+    <div class="homeAboutMe">
+        <div class="homeAboutMeInner _inner">
+            <div id="homeAboutMePic">
+                <img src="frontend/assets/img/O.png" alt="">
+            </div>
+            <div id="homeAboutMeRight">
+                <img class="tag" src="frontend/assets/img/tag.png" alt="tag.png">
+                <p>
+                    İlk, orta ve lise öğrenimimi Elazığ’da tamamladıktan sonra <b>Fırat Üniversitesi Tıp Fakültesi</b>‘ni
+                    bitirdim. Daha sonra Tıpta Uzmanlık Sınavını kazanarak yine <b>Fırat Üniversitesi Tıp Fakültesi Deri
+                        ve Zührevi Hastalıkları Anabilim Dalı</b>’nda uzmanlık eğitimime başladım.
+                    Uzmanlık eğitimini tamamladıktan sonra sırasıyla <b>Elazığ Deri ve Zührevi Hastalıklar Hastanesi</b>
+                    ve <b>Elazığ Devlet Hastanesi</b>’nde uzman doktor olarak çalıştım. Daha sonra <b>Özel Çağrı Tıp
+                        Merkezi</b> ve <b>Elazığ Medikal Park Hastaneleri</b>'nde daha sonra ise kendi kliniğimi kurarak
+                    çalışmaya devam ettim ve halen hastalarıma <b>kendi kliniğimde</b> hizmet vermeye devam etmekteyim.
+                </p>
+                <h2 class="drname">Op.Dr.Ömer Cihan Batur</h2>
+                <span class="script-text">Ortopedi ve Travmotoloji</span>
+            </div>
         </div>
     </div>
-    <h6 class="_inner">Kliniğimizde Hastalarımıza Mümkün Olan En İyi Bakımı Sunmaktan Gurur Duyuyoruz</h6>
-</div>
 
+    <div class="commonDisases">
+        <div class="container mt-5 mb-5">
+
+                <h4 class="fs-1 text-center mt-4 mb-1">Sık Karşılaşılan Rahatsızlıklar</h4>
+
+                <section id="wrapper">
+                    <div class="content">
+                        <!-- Tab links -->
+                        <div class="tabs">
+                            <button class="tablinks active" data-country="London"><p data-title="Kireçlenme">Kireçlenme</p></button>
+                            <button class="tablinks" data-country="Paris"><p data-title="Bel Fıtığı">Bel Fıtığı</p></button>
+                            <button class="tablinks" data-country="Barcelona"><p data-title="Kırık">Kırık</p></button>
+                            <button class="tablinks" data-country="Madrid"><p data-title="Kemik Erimesi">Kemik Erimesi</p></button>
+                        </div>
+
+                        <!-- Tab content -->
+                        <div class="wrapper_tabcontent">
+                            <div id="London" class="tabcontent active">
+                                <h3>Kireçlenme</h3>
+                                <p>London is the capital of Great Britain. It is one of the greatest cities in the world. It is an important business and financial centre. It is an intellectual centre, too. Everywhere in London, there are open spaces: Hyde Park and Regent Park are the largest. The City is the oldest part of London. </p>
+                            </div>
+
+                            <div id="Paris" class="tabcontent">
+                                <h3>Bel Fıtığı</h3>
+                                <p>Paris is in the Paris department of the Paris-Isle-of-France region The French historic, political and economic capital, with a population of only 2.5 million is located in the northern part of France. One of the most beautiful cities in the world. Home to historical monuments such as Notre Dame, the Eiffel tower (320m), Bastille, Louvre and many more. </p>
+                            </div>
+
+                            <div id="Barcelona" class="tabcontent">
+                                <h3>Kırık</h3>
+                                <p>Barcelona has been an urban laboratory since the high Medieval Ages. A place of diversity, a backdrop for a multiplicity of social and cultural processes on multiple scales that reflect different ways of constructing the future, a city with a long experience of urban life and social innovations. </p>
+                            </div>
+
+                            <div id="Madrid" class="tabcontent">
+                                <h3>Kemik Erimesi</h3>
+                                <p>Madrid is in the middle of Spain, in the Community of Madrid. The Community is a large area that includes the city as well as small towns and villages outside the city. 7 million people live in the Community. More than 3 million live in the city itself.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+
+        </div>
+
+    </div>
+
+    <div class="services">
+        <div class="services-inner _inner">
+            <div class="service-type">
+                <i class="fa-solid fa-brain"></i>
+                <h7 class="service-title">Lorem ipsum dolor sit amet.</h7>
+                <hr>
+                <p class="service-description">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Porro deserunt,
+                    eius at beatae cum provident?</p>
+                <ul>
+                    <li><i class="fa-solid fa-circle-check"></i> Lorem, ipsum dolor.</li>
+                    <li><i class="fa-solid fa-circle-check"></i> Lorem, ipsum dolor.</li>
+                    <li><i class="fa-solid fa-circle-check"></i> Lorem, ipsum dolor.</li>
+                </ul>
+            </div>
+
+            <div class="service-type">
+                <i class="fa-solid fa-laptop-medical"></i>
+                <h7 class="service-title">Lorem ipsum dolor sit amet.</h7>
+                <hr>
+                <p class="service-description">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Porro deserunt,
+                    eius at beatae cum provident?</p>
+                <ul>
+                    <li><i class="fa-solid fa-circle-check"></i> Lorem, ipsum dolor.</li>
+                    <li><i class="fa-solid fa-circle-check"></i> Lorem, ipsum dolor.</li>
+                    <li><i class="fa-solid fa-circle-check"></i> Lorem, ipsum dolor.</li>
+                </ul>
+            </div>
+
+            <div class="service-type">
+                <i class="fa-solid fa-house-medical"></i>
+                <h7 class="service-title">Lorem ipsum dolor sit amet.</h7>
+                <hr>
+                <p class="service-description">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Porro deserunt,
+                    eius at beatae cum provident?</p>
+                <ul>
+                    <li><i class="fa-solid fa-circle-check"></i> Lorem, ipsum dolor.</li>
+                    <li><i class="fa-solid fa-circle-check"></i> Lorem, ipsum dolor.</li>
+                    <li><i class="fa-solid fa-circle-check"></i> Lorem, ipsum dolor.</li>
+                </ul>
+            </div>
+        </div>
+        <h6 class="_inner">Kliniğimizde Hastalarımıza Mümkün Olan En İyi Bakımı Sunmaktan Gurur Duyuyoruz</h6>
+    </div>
+    <script>
+        // tabs
+
+        var tabLinks = document.querySelectorAll(".tablinks");
+        var tabContent = document.querySelectorAll(".tabcontent");
+
+
+        tabLinks.forEach(function(el) {
+            el.addEventListener("click", openTabs);
+        });
+
+
+        function openTabs(el) {
+            var btnTarget = el.currentTarget;
+            var country = btnTarget.dataset.country;
+
+            tabContent.forEach(function(el) {
+                el.classList.remove("active");
+            });
+
+            tabLinks.forEach(function(el) {
+                el.classList.remove("active");
+            });
+
+            document.querySelector("#" + country).classList.add("active");
+
+            btnTarget.classList.add("active");
+        }
+
+    </script>
 @endsection
--- resources/views/frontend/layouts/app.blade.php
+++ resources/views/frontend/layouts/app.blade.php
@@ -12,6 +12,10 @@
     <link rel="stylesheet" href="{{asset('frontend/assets/js/bootstrap.min.js')}}">
     <link rel="stylesheet" href="{{asset('frontend/assets/js/popper.min.js')}}">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+    <style>
+        @yield('style')
+    </style>
 </head>
 <body>
 <div class="header">
